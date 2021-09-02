698dc23ba99641d14b533ded90c260038cf246a2
Zac Miller-Waugh <z.millerwaugh@auckland.ac.nz>
2021-09-02 15:53:13 +1200

    Youtube embeds

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,13 +5,15 @@
 
 	import CountDown from '$lib/components/CountDown.svelte';
 	import TimeDisplay from '$lib/components/TimeDisplay.svelte';
+	import YouTube from '$lib/components/YouTube.svelte';
 
 	import { onMount } from 'svelte';
 	dayjs.extend(relativeTime);
 
-	let timeDisplayed;
+	let timeDisplayed = dayjs();
 	let isPredicted = false;
 	let loaded = false;
+	$: isInPast = timeDisplayed.isBefore(dayjs().subtract(1, 'h'));
 
 	onMount(() => {
 		(async () => {
@@ -23,7 +25,7 @@
 				console.log('Date pushed', body);
 				if (body['date_of_announcement'] !== null) {
 					timeDisplayed = dayjs(body['date_of_announcement']);
-					if (timeDisplayed.isBefore(dayjs().subtract(1, 'h'))) {
+					if (isInPast) {
 						// Give one hour leeway, then start counting down to the next one
 						timeDisplayed = timeDisplayed.add(1, 'd').hour(13);
 						isPredicted = true;
@@ -38,46 +40,117 @@
 	});
 </script>
 
-<main class:loading={!loaded}>
-	<div>
-		{#if loaded}
-			{#if typeof timeDisplayed !== 'undefined'}
-				<TimeDisplay bind:timeDisplayed />
-				<CountDown bind:timeDisplayed predicted="false" />
-			{:else}
-				<h3>Hasn't been announced yet</h3>
-			{/if}
-		{:else}
-			<BarLoader size="5" color="currentColor" unit="em" duration="1s" />
-		{/if}
+<div class="content" class:loading={!loaded}>
+	<div class="frameOuter">
+		<div class="frameInner" />
 	</div>
-</main>
+	<main>
+		<div class="countdown">
+			{#if loaded}
+				{#if typeof timeDisplayed !== 'undefined'}
+					<TimeDisplay bind:timeDisplayed />
+					<CountDown bind:timeDisplayed predicted="false" />
+				{:else}
+					<h3>Hasn't been announced yet</h3>
+				{/if}
+			{:else}
+				<BarLoader size="5" color="currentColor" unit="em" duration="1s" />
+			{/if}
+			<div class="spacer" />
+		</div>
+	</main>
+	<div class="updates">
+		<h3 class:invisible={!isInPast}>Previous updates</h3>
+		<YouTube size="lg" videoId="51i1jEFFWv8" />
+		<div class="more">
+			{#if !isInPast}<h3>Previous updates</h3>{/if}
+			<div class="gallery">
+				<YouTube videoId="51i1jEFFWv8" />
+				<YouTube videoId="51i1jEFFWv8" />
+				<YouTube videoId="51i1jEFFWv8" />
+			</div>
+		</div>
+	</div>
+</div>
 
 <style lang="scss">
-	main {
+	.content {
 		--social-distance: 4em;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		min-height: 100vh;
-		overflow: hidden;
-		margin: 0;
-		padding: 0;
-		background: repeating-linear-gradient(-45deg, #ffcc00, #ffcc00 50px, #ffff 50px, #ffff 100px);
-		div {
-			background: white;
-			width: calc(100vw - var(--social-distance));
-			height: calc(100vh - var(--social-distance));
-			transition: 0.4s ease width, 0.4s ease height;
+
+		.frameOuter {
+			position: fixed;
+			z-index: 1;
 			display: flex;
-			flex-direction: column;
 			align-items: center;
 			justify-content: center;
+			height: 100vh;
+			width: 100vw;
+			overflow: hidden;
+			margin: 0;
+			padding: 0;
+			background: repeating-linear-gradient(-45deg, #ffcc00, #ffcc00 50px, #ffff 50px, #ffff 100px);
+			.frameInner {
+				background: white;
+				width: calc(100vw - var(--social-distance));
+				height: calc(100vh - var(--social-distance));
+				transition: 0.4s ease width, 0.4s ease height;
+			}
+		}
+
+		main {
+			position: relative;
+			z-index: 10;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			flex-direction: column;
+			min-height: 100vh;
+			margin-left: calc(var(--social-distance) / 2);
+			margin-right: calc(var(--social-distance) / 2);
+			.countdown {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				text-align: center;
+			}
+
+			.spacer {
+				padding-top: 25em;
+			}
+		}
+
+		.updates {
+			position: relative;
+			z-index: 10;
+			margin-top: -35rem;
 			text-align: center;
+			background: linear-gradient(to bottom, transparent 0, white 30em);
+			padding-bottom: 5rem;
+
+			h3 {
+				font-size: 2em;
+			}
+
+			.invisible {
+				visibility: hidden;
+			}
+
+			.more {
+				padding-top: var(--social-distance);
+				.gallery {
+					display: flex;
+					align-items: center;
+					flex-wrap: wrap;
+					row-gap: 2em;
+					column-gap: 2em;
+					max-width: 66rem;
+					margin: auto;
+				}
+			}
 		}
 	}
-	main.loading {
+	.content.loading {
 		--social-distance: 16em;
 	}
 	@media (max-width: 750px) {
